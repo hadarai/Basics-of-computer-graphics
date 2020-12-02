8a847a768f0d9edec6ba3730a0b632505acffe57
max <maks.czudziak@outlook.com>
2020-12-02 18:57:42 +0100

    Some first steps of 5th tasks completed

--- Zad_5/aquarium.cpp
+++ Zad_5/aquarium.cpp
@@ -36,10 +36,9 @@ bool distance_between_points(vec3 p1, vec3 p2)
 	return pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2) + pow(p1.z - p2.z, 2);
 }
 
-void Window::secondViewPortSetup(int width, int height)
+bool is_point_still_in_aquarium(vec3 p)
 {
-	int extra_viewport_side = abs(width - height);
-	Viewport(width - extra_viewport_side, height - extra_viewport_side, extra_viewport_side, extra_viewport_side);
+	return (-1.0 < p.x && p.x < 1.0 && -1.0 < p.y && p.y < 1.0 && -1.0 < p.z && p.z < 1.0);
 }
 
 void Window::MainLoop()
@@ -47,45 +46,56 @@ void Window::MainLoop()
 	glEnable(GL_DEPTH_TEST);
 	glDepthFunc(GL_LESS);
 
-	glm::vec3 player_position = glm::vec3(-1.1, -1.1, -1.1);
+	glm::vec3 player_position = glm::vec3(-0.9, -0.9, -0.9);
 	glm::vec3 win_position = glm::vec3(1.05, 1.05, 1.05);
 	glm::vec3 new_player_position;
 
 	Sphere player_position_representation("objects/Sphere/PlayerSphereFragmentShader.fragmentshader");
 	Sphere win_sphere("objects/Sphere/WinSphereFragmentShader.fragmentshader");
+	Cube test_cube;
 
+	bool first_person_view = true;
+
+	glm::mat4 ProjectionMatrix;
+	glm::mat4 ViewMatrix;
+	glm::mat4 ModelMatrix;
+	glm::mat4 MVP_first_view;
 	do
 	{
-		int side = min(wd, ht);
 		// Clear the screen
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-		new_player_position = computeMatricesFromInputs(player_position, win());
-		glm::mat4 ProjectionMatrix = getProjectionMatrix();
-		glm::mat4 ViewMatrix = getViewMatrix();
-		glm::mat4 ModelMatrix = glm::mat4(1.0);
-		glm::mat4 MVP_first_view = ProjectionMatrix * ViewMatrix * ModelMatrix;
-		ViewportOne(0, 0, side, side);
-		win_sphere.draw(MVP_first_view, win_position);
-
-		// ==================================================================
-		secondViewPortSetup(wd, ht);
-		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-
-		computeViewMatrices(win());
+		// new_player_position = first_person_view ? computeMatricesFromInputs(player_position, win()) : computeViewMatrices(win());
+		// printf("%d\n", first_person_view);
+		if (first_person_view)
+		{
+			printf("Rysuje pierwszoosobowy\n");
+			new_player_position = computeMatricesFromInputs(player_position, win());
+		}
+		else
+		{
+			printf("Rysuje panoramiczny\n");
+			computeViewMatrices(win());
+		}
+		// player_position = computeMatricesFromInputs(player_position, win());
 		ProjectionMatrix = getProjectionMatrix();
 		ViewMatrix = getViewMatrix();
 		ModelMatrix = glm::mat4(1.0);
-		glm::mat4 MVP_second_view = ProjectionMatrix * ViewMatrix * ModelMatrix;
+		MVP_first_view = ProjectionMatrix * ViewMatrix * ModelMatrix;
 
-		player_position_representation.draw(MVP_second_view, player_position);
-		win_sphere.draw(MVP_second_view, win_position);
+		ViewportOne(0, 0, wd, ht);
+		win_sphere.draw(MVP_first_view, win_position);
+		test_cube.draw(MVP_first_view);
 
-		bool we_have_collision = false;
+		// bool we_have_collision = false;
+		// we_have_collision = is_point_still_in_aquarium(player_position);
+		// if (!we_have_collision)
+		// 	player_position = new_player_position;
 
-		if (!we_have_collision)
-			player_position = new_player_position;
-		// ==========================================================
+		if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS)
+		{
+			first_person_view = !first_person_view;
+		}
 		// Swap buffers
 		glfwSwapBuffers(window);
 		glfwPollEvents();
@@ -134,7 +144,7 @@ int main(int argc, char *argv[])
 	}
 
 	srand(seed);
-	main_window.Init(1280, 720, "Maze 3D", 0, 33);
+	main_window.Init(1280, 720, "Aquarium", 0, 33);
 	main_window.MainLoop();
 	return 0;
 }
